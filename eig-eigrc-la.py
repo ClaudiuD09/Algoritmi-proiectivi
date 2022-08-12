4f157d7eb28eeac6d7e38e87986238365ad2cf5b
ClaudiuD09 <111081576+ClaudiuD09@users.noreply.github.com>
2022-08-12 11:11:35 +0300

    Update eig-eigrc-la.py

--- eig-eigrc-la.py
+++ eig-eigrc-la.py
@@ -34,6 +34,7 @@ def nn(A,pozaCautataVect,norm):
     return pozitia
 
 def configurareA():
+	#AT&T Database of Faces
 	caleBD=r'F:\zSCOALA\acs folder\proiect\att_faces' #calea catre folder cu poze de recunoastere
 	for i in range(1,nrPersoane+1):
 		caleFolderPers=caleBD+'\s'+str(i)+'\\'
